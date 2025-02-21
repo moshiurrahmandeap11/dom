e0ce928a435a86c36a5e54fa8260327bae9d139a
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-22 01:03:46 +0600

    js dom 5 of 3 element

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,22 @@
 console.log('hello from another universe');
+console.log(document.getElementsByTagName('h1'))
+const licollection = document.getElementsByTagName('li')
+// console.log(licollection)
+
+for (let li of licollection){
+    console.log(li.innerHTML)
+}
+
+const allheadings = document.getElementsByTagName('h1')
+for (let h1 of allheadings){
+    console.log(h1.innerText)
+}
+
+
+const firstID = document.getElementById("dom-title");
+console.log(firstID.innerText = 'moshiur list')
+
+const firstClass = document.getElementsByClassName('first-class')
+for (let first of firstClass){
+    console.log(first.innerText)
+}
