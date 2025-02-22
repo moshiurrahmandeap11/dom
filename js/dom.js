eaf44130db19b4718afc8fc3114a66fa11730eee
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-23 02:27:32 +0600

    js add

--- js/dom.js
+++ js/dom.js
@@ -1,22 +1,34 @@
-console.log('hello from another universe');
-console.log(document.getElementsByTagName('h1'))
+// console.log('hello from another universe');
+// console.log(document.getElementsByTagName('h1'))
 const licollection = document.getElementsByTagName('li')
 // console.log(licollection)
 
 for (let li of licollection){
-    console.log(li.innerHTML)
+    // console.log(li.innerHTML)
 }
 
 const allheadings = document.getElementsByTagName('h1')
 for (let h1 of allheadings){
-    console.log(h1.innerText)
+    // console.log(h1.innerText)
 }
 
 
 const firstID = document.getElementById("dom-title");
-console.log(firstID.innerText = 'moshiur list')
+// console.log(firstID.innerText = 'moshiur list')
 
 const firstClass = document.getElementsByClassName('first-class')
 for (let first of firstClass){
-    console.log(first.innerText)
+    // console.log(first.innerText)
 }
+
+
+const queryID = document.querySelectorAll('.dom_class li')
+// console.log(queryID)
+
+for (const iD of queryID){
+    // console.log(iD.innerText)
+}
+
+const queryClass = document.querySelector('.dom_class li');
+console.log(queryClass);
+
